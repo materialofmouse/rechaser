60e4537e921892b4a03e91210fae8b2fd5c9ecbc
materialofmouse <materialofmouse@gmail.com>
2021-06-10 23:31:36 +0900

    add:POSTメソッド, JSONのパーサ

--- connect/connect.go
+++ connect/connect.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	//"net/http"
+	//"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type RequestData struct {
+	Data struct {
+		Session string `json:"session"`
+		Command struct {
+			Action string `json:"action"`
+			Direction string `json:"direction"`
+		} `json:"command"`
+	} `json:"data"`
+}
 
 func main() {
 	r := gin.Default()
@@ -15,7 +27,16 @@ func main() {
 	})
 
 	r.POST("/post", func(c *gin.Context) {
-		fmt.Println(c.String)
+		fmt.Println("post")
+		var request RequestData
+		err := c.Bind(&request)
+		if err != nil {
+			fmt.Println(err)
+			c.Status(http.StatusBadRequest)
+		}
+		fmt.Println(request.Data.Session)
+		fmt.Println(request.Data.Command.Action)
+	})
 
 	r.Run()
 }
